3def10afe22e0c672b1e568792e9496009c1b297
AlanenR <riikka.alanen@helsinki.fi>
2023-09-21 20:11:56 +0300

    Add users table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE user_roles (
     id serial PRIMARY KEY,
     role_name VARCHAR (50) UNIQUE NOT NULL
 );
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role_id INTEGER REFERENCES user_roles(id)
+);
