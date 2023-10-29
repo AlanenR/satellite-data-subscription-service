ad5da9cbd9d5fa685d4f238f28cda1481d08bfe8
AlanenR <riikka.alanen@helsinki.fi>
2023-10-29 14:21:21 +0200

    Add session and some initial login logic plus conditional buttons in nav

--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import redirect, render_template, request, session
+from os import getenv
+
+app.secret_key = getenv("SECRET_KEY")
 
 
 @app.route("/")
@@ -7,14 +10,27 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # TODO: check username and password
+        session["username"] = username
+        return redirect("/")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/sign-up")
 def signUp():
     return render_template("sign-up.html")
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 
 @app.route("/admin-panel")
